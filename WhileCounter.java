83f96e7ccb9576ab1b9389535ddad6fb340c3897
patience gwafan <pashgwafs@gmail.com>
2020-07-14 13:53:06 +0100

    my first java project

---
+++ WhileCounter.java
@@ -0,0 +1,15 @@
+
+public class WhileCounter {
+
+	public static void main(String[] args) {
+		int counter = 1;
+		while(counter<=10);{
+		counter = counter +1;
+		++counter;
+		}
+		 System.out.println(counter);
+		}
+     
+	}
+
+
